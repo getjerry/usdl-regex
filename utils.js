ea8567a762afbb1821f670669867f493d5612813
Amogh Nayak <amoghnk03@gmail.com>
2021-01-26 18:52:17 -0500

    add rule for all zeros

---
+++ utils.js
@@ -0,0 +1,11 @@
+/**
+ * Returns a string where all the zeros are removed
+ */
+function stripZeros(number) {
+    return number.replace(/0/g, '');
+  }
+  
+  module.exports = {
+    stripZeros,
+  };
+  
